5b20ff938f5d30aef08b3718cbe72b0a3157bc5d
Mandy <66321924+mandy-mp@users.noreply.github.com>
2023-05-25 17:02:02 +0200

    added devExtreme

---
+++ Spacebook/Controllers/CreateController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Spacebook.Interfaces;
+
+namespace Spacebook.Controllers
+{
+    public class CreateController : Controller
+    {
+        private readonly IAzureBlobStorageService storageService;
+
+        public CreateController(IAzureBlobStorageService storageService) 
+        { 
+            this .storageService = storageService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(IFormFile file) 
+        {
+            string userId = "test";
+            // use this to save to database
+            var fileURI = storageService.UploadBlob(file, userId);
+            Console.WriteLine(fileURI);
+
+            storageService.GetAllBlobs("test");
+
+            return View(file);
+        }
+    }
+}
